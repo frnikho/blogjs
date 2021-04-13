98b519342b060c7510a55dd31aadbdc5a64e6626
Nicolas SANS <nicolas.sans@epitech.eu>
2021-04-13 15:39:11 +0200

    edit jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    testEnvironment: "node",
+    collectCoverage: true,
+    testResultsProcessor: "jest-sonar-reporter",
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "/tests/",
+        "/.next/"
+    ]
 };
